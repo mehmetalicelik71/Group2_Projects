e6b3fab6f941bccb2ab7369de7d5a6f10126cd3d
Mehmet Ali Celik <malicelk71@gmail.com>
2022-10-11 22:51:55 +0300

    Group2 Projects have been created.

---
+++ src/day07/Variables.java
@@ -0,0 +1,16 @@
+package day07;
+
+public class Variables {
+    public static void main(String[] args) {
+        //=(assignment) operatörü sağında bulunan sabit bir değer (Literal) veya işlemin sonucunu
+        // solundaki değişkenin (Variable) temsil ettiği hafıza bölgesine kaydeder.
+        int yas=40; //bu satır bir java ifadesidir (statement)
+
+
+        //Karenin alanını ve çevresini hesaplayınız.
+        int kenar=5;
+        System.out.println(kenar*kenar);
+        System.out.println(4*kenar);
+
+    }
+}
